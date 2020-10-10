51a84b56b889ee969abafcd7f00b43745d93ad28
Saszr <saszrlsh@gmail.com>
2020-10-10 13:53:51 +0800

    update: 泰康投资Demo-集团项目总览-项目组合分析

--- maim/泰康/集团项目总览/集团项目总览.js
+++ maim/泰康/集团项目总览/集团项目总览.js
@@ -1,5 +1,308 @@
 var Cus_theme = 'westeros';
 var Cus_echarts = {};
+
+function ROI() {
+  let cardName = 'ROI';
+  let cardName_mini = 'ROI_mini';
+  let echartDom = cfs.card.body.getDom(cardName).find('.echart');
+  let headDom = cfs.card.head.getDom(cardName);
+
+  headDom.find('.card-header').css('display', 'none');
+  cfs.card.body.getDom(cardName).css('padding', '0');
+
+  // echart div高度
+  let cardBodyDom = cfs.card.body.getDom(cardName);
+  let _height = $(cardBodyDom).height();
+  $(echartDom).height(_height);
+
+  $(echartDom).css('background-color', '#26a69a');
+
+  // 增加容器dom
+  let Wrapper = `
+  <div style="height:50%;padding:1rem;color:white">
+  <div class="d-flex">
+  <div>
+    <span class="font-size-sm opacity-75 mr-2">2020 实际</span>
+    <span style="font-size: 1.3rem">10,424,890</span>
+  </div>
+  <span class="badge bg-teal-800 badge-pill align-self-center ml-auto">18.11%</span>
+</div>
+<div class="d-flex">
+  <div>
+    <span class="font-size-sm opacity-75 mr-2">2021 预算</span>
+    <span style="font-size: 1.3rem">12,312,843</span>
+  </div>
+  <span class="align-self-center ml-auto" style="font-size: 1.3rem">销售收入</span>
+</div>
+  </div>
+    <div id="${cardName_mini}" style="width:100%;position: absolute;bottom: 0;height: 50%"></div>
+  `;
+
+  echartDom.html(Wrapper);
+
+  let miniEchartDom = cfs.card.body.getDom(cardName).find(`#${cardName_mini}`);
+
+  let option = {
+    grid: {
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+    },
+    xAxis: {
+      show: false,
+      splitLine: { show: false },
+      type: 'category',
+      data: [1, 2, 3, 4, 5, 6, 7],
+    },
+    yAxis: {
+      show: false,
+      splitLine: { show: false },
+      type: 'value',
+      max: function (value) {
+        return value.max + 0.5;
+      },
+    },
+    series: [
+      {
+        name: '直接访问',
+        type: 'bar',
+        itemStyle: {
+          normal: {
+            color: "rgba(255,255,255,0.5)",
+          },
+        },
+        data: [10, 52, 200, 334, 390, 330, 220],
+      },
+    ],
+  };
+
+  if (!Cus_echarts[cardName_mini]) {
+    Cus_echarts[cardName_mini] = cfs.echarts.init(miniEchartDom, Cus_theme, option);
+  } else {
+    cfs.echarts.refresh(Cus_echarts[cardName_mini], option);
+  }
+}
+
+function ROE() {
+  let cardName = 'ROE';
+  let cardName_mini = 'ROE_mini';
+  let echartDom = cfs.card.body.getDom(cardName).find('.echart');
+  let headDom = cfs.card.head.getDom(cardName);
+
+  headDom.find('.card-header').css('display', 'none');
+  cfs.card.body.getDom(cardName).css('padding', '0');
+
+  // echart div高度
+  let cardBodyDom = cfs.card.body.getDom(cardName);
+  let _height = $(cardBodyDom).height();
+  $(echartDom).height(_height);
+
+  $(echartDom).css('background-color', '#ec407a');
+
+  // 增加容器dom
+  let Wrapper = `
+    <div style="height:50%;padding:1rem;color:white">
+    <div class="d-flex">
+    <div>
+      <span class="font-size-sm opacity-75 mr-2">2020 实际</span>
+      <span style="font-size: 1.3rem">45,279</span>
+    </div>
+    <span class="badge bg-teal-800 badge-pill align-self-center ml-auto">+5.59%</span>
+  </div>
+  <div class="d-flex">
+    <div>
+      <span class="font-size-sm opacity-75 mr-2">2021 预算</span>
+      <span style="font-size: 1.3rem">45,279</span>
+    </div>
+    <span class="align-self-center ml-auto" style="font-size: 1.3rem">销售数量</span>
+  </div>
+    </div>
+    <div id="${cardName_mini}" style="width:100%;position: absolute;bottom: 0;height: 50%"></div>
+  `;
+
+  echartDom.html(Wrapper);
+
+  let miniEchartDom = cfs.card.body.getDom(cardName).find(`#${cardName_mini}`);
+
+  let option = {
+    grid: {
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+    },
+    xAxis: {
+      show: false,
+      splitLine: { show: false },
+      type: 'category',
+      data: [1, 2, 3, 4, 5, 6, 7],
+    },
+    yAxis: {
+      show: false,
+      splitLine: { show: false },
+      type: 'value',
+      max: function (value) {
+        return value.max + 0.5;
+      },
+    },
+    series: [
+      {
+        name: '直接访问',
+        type: 'bar',
+        itemStyle: {
+          normal: {
+            color: "rgba(255,255,255,0.5)",
+          },
+        },
+        data: [10, 52, 200, 334, 390, 330, 220],
+      },
+    ],
+  };
+
+  if (!Cus_echarts[cardName_mini]) {
+    Cus_echarts[cardName_mini] = cfs.echarts.init(miniEchartDom, Cus_theme, option);
+  } else {
+    cfs.echarts.refresh(Cus_echarts[cardName_mini], option);
+  }
+}
+
+function IRR() {
+  let cardName = 'IRR';
+  let cardName_mini = 'IRR_mini';
+  let echartDom = cfs.card.body.getDom(cardName).find('.echart');
+  let headDom = cfs.card.head.getDom(cardName);
+
+  headDom.find('.card-header').css('display', 'none');
+  cfs.card.body.getDom(cardName).css('padding', '0');
+
+  // echart div高度
+  let cardBodyDom = cfs.card.body.getDom(cardName);
+  let _height = $(cardBodyDom).height();
+  $(echartDom).height(_height);
+
+  $(echartDom).css('background-color', '#29b6f6');
+
+  // 增加容器dom
+  let Wrapper = `
+  <div style="height:50%;padding:1rem;color:white">
+  <div class="d-flex">
+  <div>
+    <span class="font-size-sm opacity-75 mr-2">2020 实际</span>
+    <span style="font-size: 1.3rem">313</span>
+  </div>
+  <span class="badge bg-teal-800 badge-pill align-self-center ml-auto">11.02%</span>
+</div>
+<div class="d-flex">
+  <div>
+    <span class="font-size-sm opacity-75 mr-2">2021 预算</span>
+    <span style="font-size: 1.3rem">347.50</span>
+  </div>
+  <span class="align-self-center ml-auto" style="font-size: 1.3rem">吊牌单价</span>
+</div>
+  </div>
+    <div id="${cardName_mini}" style="width:100%;position: absolute;bottom: 0;height: 50%"></div>
+  `;
+
+  echartDom.html(Wrapper);
+
+  let miniEchartDom = cfs.card.body.getDom(cardName).find(`#${cardName_mini}`);
+
+  let option = {
+    grid: {
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+    },
+    xAxis: {
+      show: false,
+      splitLine: { show: false },
+      type: 'category',
+      data: [1, 2, 3, 4, 5, 6, 7],
+    },
+    yAxis: {
+      show: false,
+      splitLine: { show: false },
+      type: 'value',
+      max: function (value) {
+        return value.max + 0.5;
+      },
+    },
+    series: [
+      {
+        name: '直接访问',
+        type: 'bar',
+        itemStyle: {
+          normal: {
+            color: "rgba(255,255,255,0.5)",
+          },
+        },
+        data: [10, 52, 200, 334, 390, 330, 220],
+      },
+    ],
+  };
+
+  if (!Cus_echarts[cardName_mini]) {
+    Cus_echarts[cardName_mini] = cfs.echarts.init(miniEchartDom, Cus_theme, option);
+  } else {
+    cfs.echarts.refresh(Cus_echarts[cardName_mini], option);
+  }
+}
+
+function Chart_R3() {
+  let cardName = 'Chart_R3';
+  let echartDom = cfs.card.body.getDom(cardName).find('.echart');
+  let headDom = cfs.card.head.getDom(cardName);
+
+  let option = {
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      data: ['累计入市', '累计入住'],
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+      },
+    },
+    xAxis: {
+      type: 'category',
+      data: [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: '累计入市',
+        type: 'line',
+        stack: '总量',
+        data: [41, 80, 85, 108, 112, 134, 158, 178, 200, 219, 249],
+      },
+      {
+        name: '累计入住',
+        type: 'line',
+        stack: '总量',
+        data: [17, 34, 74, 94, 104, 118, 136, 154, 168, 178, 189],
+      },
+    ],
+  };
+
+  if (!Cus_echarts[cardName]) {
+    Cus_echarts[cardName] = cfs.echarts.init(echartDom, Cus_theme, option);
+  } else {
+    cfs.echarts.refresh(Cus_echarts[cardName], option);
+  }
+}
+
 function Chart_R1C1_Main() {
   var cardName = 'Chart_R1C1';
   var echartDom = cfs.card.body.getDom(cardName).find('.echart');
@@ -27,8 +330,8 @@ function Chart_R1C1_Main() {
       {
         name: '',
         type: 'pie',
-        center: ['54%', '50%'],
-        // radius: '30%',
+        center: ['54%', '60%'],
+        radius: '55%',
         data: pieData.reverse(),
         itemStyle: {
           emphasis: {
@@ -39,7 +342,7 @@ function Chart_R1C1_Main() {
           normal: {
             label: {
               formatter: function (params) {
-                return params.name + '(' + params.percent + '%)';
+                return params.percent + '%';
               },
             },
             labelLine: {
@@ -207,154 +510,92 @@ function Chart_R2C1_Main() {
   var echartDom = cfs.card.body.getDom(cardName).find('.echart');
   var headDom = cfs.card.head.getDom(cardName);
   headDom.find('.freshBS').find('i').css('margin', 10);
-  var data = {
-    axis: ['项目B', '项目D', '项目A', '项目C', '项目F', '项目E'],
-    series: [{ name: '前十项目投资', data: [{ name: '项目金额', value: [932560.232382, 735000, 331923.574457, 312515.029877, 186905.806274, 76683.558963] }] }],
-  };
-  var yData = data.series[0].data;
-  var yDataHead = data.axis;
-  var yDataHead = yDataHead.reverse();
-  var yDataValue1 = yData[0].value;
-  var yDataValue1 = yDataValue1.reverse();
-  var option = {
+
+  let option = {
     tooltip: {
       trigger: 'axis',
       axisPointer: {
         // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow',
-      },
-      formatter: function (params) {
-        var html = '';
-        if (params.length > 0) {
-          for (var int = 0; int < params.length; int++) {
-            html += params[int].axisValue + '<br>' + params[int].marker + params[int].seriesName + '：' + format((params[int].data / 1000).toFixed(2)) + '<br>';
-          }
-        }
-        return html;
+        type: 'shadow', // 默认为直线，可选为：'line' | 'shadow'
       },
     },
+    legend: {
+      data: ['华北', '华东', '华中', '华南'],
+    },
     grid: {
-      left: '2%',
-      right: 75,
-      bottom: '10%',
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
       containLabel: true,
     },
     xAxis: {
       type: 'value',
-      name: getLanguage('money2') + '：千元',
-      // boundaryGap: [0, 0.01],
       axisLabel: {
-        formatter: function (val) {
-          return format(val / 1000);
+        formatter: function (value) {
+          var res = value;
+          return res + '%';
         },
-        interval: 0,
-        rotate: 40,
       },
     },
     yAxis: {
       type: 'category',
-      data: yDataHead,
-      // interval:0,
-      axisLabel: {
-        interval: 0,
-        rotate: 20,
-      },
+      data: ['净利润', '其他费用', '委托管理费', '销售费用', '管理费用', '主营业务成本'],
     },
     series: [
       {
-        name: yData[0].name,
+        name: '华北',
         type: 'bar',
         stack: '总量',
-        barWidth: 25,
         label: {
-          normal: {
-            show: false,
-            position: 'insideRight',
+          show: true,
+          position: 'insideRight',
+          formatter: function (name) {
+            return name['data'] + '%';
           },
         },
-        data: yDataValue1,
-        itemStyle: {
-          normal: {
-            color: '#569ADA',
+        data: [20, 4, 8, 16, 16, 36],
+      },
+      {
+        name: '华东',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          show: true,
+          position: 'insideRight',
+          formatter: function (name) {
+            return name['data'] + '%';
           },
         },
+        data: [29, 2, 6, 17, 16, 30],
+      },
+      {
+        name: '华中',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          show: true,
+          position: 'insideRight',
+          formatter: function (name) {
+            return name['data'] + '%';
+          },
+        },
+        data: [13, 3, 5, 17, 17, 45],
+      },
+      {
+        name: '华南',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          show: true,
+          position: 'insideRight',
+          formatter: function (name) {
+            return name['data'] + '%';
+          },
+        },
+        data: [15, 2, 7, 16, 18, 42],
       },
     ],
   };
-
-  // 手机端
-  if (getRequest().isView == 'mobile') {
-    // 高度调整
-    cfs.echarts.mobileHeight(cardName, 340);
-
-    // 图表配置修改
-    option = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          // 坐标轴指示器，坐标轴触发有效
-          type: 'shadow',
-        },
-        formatter: function (params) {
-          var html = '';
-          if (params.length > 0) {
-            for (var int = 0; int < params.length; int++) {
-              html += params[int].axisValue + '<br>' + params[int].marker + params[int].seriesName + '：' + format((params[int].data / 1000).toFixed(2)) + '<br>';
-            }
-          }
-          return html;
-        },
-      },
-      grid: {
-        left: '1%',
-        right: '1%',
-        bottom: '1%',
-        top: '1%',
-        containLabel: true,
-      },
-      xAxis: {
-        type: 'value',
-        name: getLanguage('money2') + '：千元',
-        // boundaryGap: [0, 0.01],
-        axisLabel: {
-          formatter: function (val) {
-            return format(val / 1000);
-          },
-          interval: 0,
-          rotate: 40,
-        },
-      },
-      yAxis: {
-        type: 'category',
-        data: yDataHead,
-        // interval:0,
-        axisLabel: {
-          interval: 0,
-          rotate: 20,
-        },
-      },
-      series: [
-        {
-          name: yData[0].name,
-          type: 'bar',
-          stack: '总量',
-          barWidth: 15,
-          label: {
-            normal: {
-              show: false,
-              position: 'insideRight',
-            },
-          },
-          data: yDataValue1,
-          itemStyle: {
-            normal: {
-              color: '#569ADA',
-            },
-          },
-        },
-      ],
-    };
-  }
 
   if (!Cus_echarts[cardName]) {
     Cus_echarts[cardName] = cfs.echarts.init(echartDom, Cus_theme, option);
@@ -368,133 +609,24 @@ function Chart_R2C2_Main() {
   var echartDom = cfs.card.body.getDom(cardName).find('.echart');
   var headDom = cfs.card.head.getDom(cardName);
   headDom.find('.freshBS').find('i').css('margin', 10);
-  var data = {
-    series: [
-      {
-        name: 'ROI排名',
-        data: [
-          { description_1: '项目A', StaffAdjInvest: 0.211 },
-          { description_1: '项目C', StaffAdjInvest: 0.077 },
-          { description_1: '项目B', StaffAdjInvest: 0.061 },
-          { description_1: '项目F', StaffAdjInvest: 0.055 },
-          { description_1: '项目E', StaffAdjInvest: 0.025 },
-          { description_1: '项目D', StaffAdjInvest: 0.021 },
-        ],
-      },
-    ],
-  };
-  let yData = data.series[0];
-  let yDataValue = [];
-  yData.data.forEach(function (v) {
-    yDataValue.push(numPro(v['StaffAdjInvest']));
-  });
-  let xDataHead = [];
-  yData.data.forEach(function (v) {
-    xDataHead.push(v['description_1']);
-  });
-  option = {
-    tooltip: {
-      trigger: 'axis',
-      formatter: function (name) {
-        return name[0].data + '%';
-      },
-    },
-    xAxis: {
-      type: 'category',
-      data: xDataHead,
-      // triggerEvent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: 40,
-      },
-    },
-    grid: {
-      left: '10%',
-      right: '4%',
-      bottom: '1%',
-      containLabel: true,
-    },
-    yAxis: {
-      type: 'value',
-      axisLabel: {
-        formatter: function (value) {
-          var res = value;
-          return res + '%';
-        },
-      },
-    },
-    series: [
-      {
-        data: yDataValue,
-        type: 'bar',
-        label: {
-          normal: {
-            show: true,
-            position: 'top',
-            formatter: function (name) {
-              return name['data'] + '%';
-            },
-          },
-        },
-      },
-    ],
-  };
 
-  // 手机端
-  if (getRequest().isView == 'mobile') {
-    // 高度调整
-    cfs.echarts.mobileHeight(cardName, 340);
-
-    // 图表配置修改
-    option = {
-      tooltip: {
-        trigger: 'axis',
-        formatter: function (name) {
-          return name[0].data + '%';
-        },
-      },
-      xAxis: {
-        type: 'category',
-        data: xDataHead,
-        // triggerEvent: true,
-        axisLabel: {
-          interval: 0,
-          rotate: 40,
-        },
-      },
-      grid: {
-        left: '1%',
-        right: '1%',
-        top: '5%',
-        bottom: '1%',
-        containLabel: true,
-      },
-      yAxis: {
-        type: 'value',
-        axisLabel: {
-          formatter: function (value) {
-            var res = value;
-            return res + '%';
-          },
-        },
-      },
-      series: [
-        {
-          data: yDataValue,
-          type: 'bar',
-          label: {
-            normal: {
-              show: true,
-              position: 'top',
-              formatter: function (name) {
-                return name['data'] + '%';
-              },
-            },
-          },
-        },
+  let option = {
+    legend: {},
+    tooltip: {},
+    dataset: {
+      source: [
+        ['product', '华北', '华东', '华中', '华南'],
+        ['收入', 703.0, 705.0, 580.0, 332.0],
+        ['成本', 414.77, 408.9, 319.0, 205.84],
+        ['近利润', 288.23, 296.1, 261.0, 126.16],
       ],
-    };
-  }
+    },
+    xAxis: { type: 'category' },
+    yAxis: {},
+    // Declare several bar series, each will be mapped
+    // to a column of dataset.source by default.
+    series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }],
+  };
 
   if (!Cus_echarts[cardName]) {
     Cus_echarts[cardName] = cfs.echarts.init(echartDom, Cus_theme, option);
@@ -1073,8 +1205,9 @@ function Chart_R4_Main() {
     },
     grid: {
       left: '2%',
-      right: 75,
-      bottom: '10%',
+      right: '2%',
+      bottom: '13%',
+      top: '5',
       containLabel: true,
     },
     dataZoom: [
@@ -1101,15 +1234,15 @@ function Chart_R4_Main() {
           rotate: 40,
         },
       },
-      {
-        type: 'value',
-        min: 0,
-        max: 8.22,
-        // interval: 25,
-        axisLabel: {
-          formatter: '{value} %',
-        },
-      },
+      // {
+      //   type: 'value',
+      //   min: 0,
+      //   max: 8.22,
+      //   // interval: 25,
+      //   axisLabel: {
+      //     formatter: '{value} %',
+      //   },
+      // },
     ],
     xAxis: {
       type: 'category',
