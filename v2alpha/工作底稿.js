8bd08e189f2c94f00af10733bdeb6e9931cd15c3
Saszr <saszrlsh@gmail.com>
2021-05-07 15:49:24 +0800

    update

--- v2alpha/工作底稿.js
+++ v2alpha/工作底稿.js
@@ -1,15 +1,57 @@
-function abc() {
-  spread.suspendPaint();
-  var sheet = spread.getActiveSheet();
-  try {
-    sheet.columnOutlines.ungroupRange(2, 6); //取消列分组初始化
-    sheet.columnOutlines.group(2, 6); //C-H列区域分组
-    sheet.columnOutlines.direction(GC.Spread.Sheets.Outlines.OutlineDirection.backward); //按钮位置放于左侧
-    sheet.columnOutlines.expand(0, false); //列上的区域分组默认收起
-  } catch (e) {
-    sheet.invalidateLayout();
-    sheet.repaint(); //当前sheet重新绘制
-  }
+const { spread, GC, setPovChangeCallback, povForm, dispatchDataSheetInfo } = params;
 
-  spread.resumePaint();
+const handleInit = () => {
+  const configureBtnWrap = document.querySelector("#configureBtnWrap");
+
+  let linkTo = document.createElement("button");
+  linkTo.setAttribute("class", "ant-btn ant-btn-sm");
+  linkTo.setAttribute("type", "button");
+  linkTo.innerHTML = "<span>跳转</span>";
+  configureBtnWrap.prepend(linkTo);
+
+  linkTo.onclick = () => {
+    const sheet = spread.getActiveSheet();
+    const activeColumnIndex = sheet.getActiveColumnIndex();
+    const activeRowIndex = sheet.getActiveRowIndex();
+
+    const cellVal = sheet.getCell(activeRowIndex, activeColumnIndex).value();
+
+    if (cellVal === null) {
+      // console.log('单元格为 null')
+    } else {
+      const domain = window.location.host;
+      let paramObj = getUrlParams();
+
+      switch (activeColumnIndex) {
+        case 19:
+          window.open(`form-list-view?id=LSTECB0H32MPCI&name=JournalEntry_List&sign=${paramObj.sign}`);
+          break;
+        case 20:
+          window.open(`form-list-view?id=LSTECH4QBI8Q08&name=Reconciliation_report&sign=${paramObj.sign}`);
+          break;
+        case 21:
+          window.open(`form-list-view?id=LSTECGD78BL927&sign=${paramObj.sign}`);
+          break;
+      }
+    }
+  };
+};
+
+function getUrlParams() {
+  let arrayTemp = window.location.href.split("?");
+  let obj = new Object();
+  //如果不带参数，则不执行下面的代码
+  if (arrayTemp.length > 1) {
+    let params = arrayTemp[1].split("&");
+    for (let i = 0; i < params.length; i++) {
+      let parm = params[i].split("=");
+      //将key和value定义给obj
+      obj[parm[0]] = parm[1];
+    }
+  }
+  return obj;
 }
+
+return {
+  handleInit,
+};
